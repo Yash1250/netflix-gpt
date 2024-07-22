7b4ae27a2c04b72f3cc571371b4c02016ce5372c
Yash Mehta <mehtayashkuleri503@gmail.com>
2024-07-22 18:00:34 +0530

    Added Main Movie Trailer Container and Description

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -3,9 +3,18 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovie from "../hooks/useNowPlayingMovie";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovie();
+  return (
+    <div>
+      <Header />
+      <MainContainer />
+      <SecondaryContainer />
+    </div>
+  );
 };
 
 export default Browse;
---
+++ src/hooks/useMainMovie.js
@@ -0,0 +1,32 @@
+/** @format */
+
+import { useEffect, useState } from "react";
+import { options } from "../utils/constants";
+
+const useMainMovie = (movieId) => {
+  const [trailer, setTrailer] = useState("");
+
+  const fetchMainMovieVideo = async () => {
+    const response = await fetch(
+      `https://api.themoviedb.org/3/movie/${movieId}/videos?`,
+      options
+    );
+    const json = await response.json();
+    const trailers = json.results.filter((elem) => elem?.type === "Trailer");
+
+    if (trailers.length > 0) {
+      setTrailer(
+        `https://www.youtube.com/embed/${trailers[0].key}?autoplay=1&mute=0&si=PWRt5E1A-nywPyYt`
+        //`https://www.youtube.com/embed/${trailers[0].key}?autoplay=1&mute=1&controls=0&modestbranding=1&rel=0&iv_load_policy=3&fs=0`
+      );
+    }
+  };
+
+  useEffect(() => {
+    fetchMainMovieVideo();
+  }, [movieId]);
+
+  return trailer;
+};
+
+export default useMainMovie;
